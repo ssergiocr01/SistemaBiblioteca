f09495cd91b6a099d769771ff68463b077bb72db
sserrano <ssergio0@hotmail.com>
2023-05-12 08:41:23 -0600

    Creando Proyecto

---
+++ SistemaBiblioteca/Server/Models/DbbibliotecaBlazorContext.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaBiblioteca.Server.Models;
+
+public partial class DbbibliotecaBlazorContext : DbContext
+{
+    public DbbibliotecaBlazorContext()
+    {
+    }
+
+    public DbbibliotecaBlazorContext(DbContextOptions<DbbibliotecaBlazorContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categoria { get; set; }
+
+    public virtual DbSet<EstadoPrestamo> EstadoPrestamos { get; set; }
+
+    public virtual DbSet<Lector> Lectors { get; set; }
+
+    public virtual DbSet<Libro> Libros { get; set; }
+
+    public virtual DbSet<NumeroCorrelativo> NumeroCorrelativos { get; set; }
+
+    public virtual DbSet<Prestamo> Prestamos { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("PK__Categori__A3C02A106C644BF6");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Estado).HasDefaultValueSql("((1))");
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<EstadoPrestamo>(entity =>
+        {
+            entity.HasKey(e => e.IdEstadoPrestamo).HasName("PK__EstadoPr__BCB87549039E7EC2");
+
+            entity.ToTable("EstadoPrestamo");
+
+            entity.Property(e => e.IdEstadoPrestamo).ValueGeneratedNever();
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Estado).HasDefaultValueSql("((1))");
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<Lector>(entity =>
+        {
+            entity.HasKey(e => e.IdLector).HasName("PK__Lector__9644AB8B78D7F54F");
+
+            entity.ToTable("Lector");
+
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Codigo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Estado).HasDefaultValueSql("((1))");
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Libro>(entity =>
+        {
+            entity.HasKey(e => e.IdLibro).HasName("PK__Libro__3E0B49ADA7438735");
+
+            entity.ToTable("Libro");
+
+            entity.Property(e => e.Autor)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Editorial)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Estado).HasDefaultValueSql("((1))");
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Titulo)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Ubicacion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.Libros)
+                .HasForeignKey(d => d.IdCategoria)
+                .HasConstraintName("FK__Libro__IdCategor__4D94879B");
+        });
+
+        modelBuilder.Entity<NumeroCorrelativo>(entity =>
+        {
+            entity.HasKey(e => e.IdNumeroCorrelativo).HasName("PK__NumeroCo__84369489DD0C88BB");
+
+            entity.ToTable("NumeroCorrelativo");
+
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Prefijo)
+                .HasMaxLength(2)
+                .IsUnicode(false);
+            entity.Property(e => e.Tipo)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Prestamo>(entity =>
+        {
+            entity.HasKey(e => e.IdPrestamo).HasName("PK__Prestamo__6FF194C04FB10182");
+
+            entity.ToTable("Prestamo");
+
+            entity.Property(e => e.Codigo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Estado).HasDefaultValueSql("((1))");
+            entity.Property(e => e.EstadoEntregado)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.EstadoRecibido)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.FechaConfirmacionDevolucion).HasColumnType("datetime");
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.FechaDevolucion).HasColumnType("datetime");
+
+            entity.HasOne(d => d.IdEstadoPrestamoNavigation).WithMany(p => p.Prestamos)
+                .HasForeignKey(d => d.IdEstadoPrestamo)
+                .HasConstraintName("FK__Prestamo__IdEsta__5CD6CB2B");
+
+            entity.HasOne(d => d.IdLectorNavigation).WithMany(p => p.Prestamos)
+                .HasForeignKey(d => d.IdLector)
+                .HasConstraintName("FK__Prestamo__IdLect__5DCAEF64");
+
+            entity.HasOne(d => d.IdLibroNavigation).WithMany(p => p.Prestamos)
+                .HasForeignKey(d => d.IdLibro)
+                .HasConstraintName("FK__Prestamo__IdLibr__5EBF139D");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__645723A6E6589DC8");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+            entity.Property(e => e.Clave)
+                .HasMaxLength(40)
+                .IsUnicode(false)
+                .HasColumnName("clave");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(40)
+                .IsUnicode(false)
+                .HasColumnName("correo");
+            entity.Property(e => e.EsActivo).HasColumnName("esActivo");
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.NombreApellidos)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("nombreApellidos");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
